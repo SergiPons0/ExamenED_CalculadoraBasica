78e5ff183ae1855fb5c96b841da1b29c7421ca4e
SergiPons <spons20472@iesjoanramis.org>
2024-12-12 18:53:34 +0100

    Programa con comentarios Javadoc

--- src/calculadora/SumaNumeros.java
+++ src/calculadora/SumaNumeros.java
@@ -1,17 +1,21 @@
 package calculadora;
 import java.util.Scanner;
-
+// @author SergiPons
+// @version 0.0
 public class SumaNumeros {
 	public static void main(String[] args) {
+			// <h2>Programa Calculadora Basica<h2>
+			// <h3> Clase de Suma de Numeros<h2>
+		
 			Scanner scanner = new Scanner(System.in);
-			// Pedimos al usuario que ingrese un numero
+			// <p>Pedimos al usuario que ingrese un numero<p>
 			System.out.print("Ingresa el primer número: ");
 			int numero1 = scanner.nextInt();
-			// Pedimos al usuario que ingrese otro numero
+			// <p>Pedimos al usuario que ingrese otro numero<p>
 			System.out.print("Ingresa el segundo número: ");
 			int numero2 = scanner.nextInt();
 			int suma = numero1 + numero2;
-			// Imprimimos la suma de los dos numeros introducidos por el usuario
+			//<p> Imprimimos la suma de los dos numeros introducidos por el usuario<p>
 			System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
 			
 			// Cambio realizado desde rama feature-suma
